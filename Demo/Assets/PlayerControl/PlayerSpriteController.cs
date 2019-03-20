075d90f28570ef75db03f555e97dc3c8b9ef6f21
Jose Calderin <43352611+josecalderin@users.noreply.github.com>
2019-03-20 19:40:23 -0400

    Game Code
    Movement done the jumping working and the finishing the combat system

---
+++ Demo/Assets/PlayerControl/PlayerSpriteController.cs
@@ -0,0 +1,73 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerSpriteController : MonoBehaviour {
+
+    public float moveSpeed;
+    Animator anim;
+    Rigidbody2D playerRigidBody;   
+    bool playerMoving;
+    Vector2 lastMove;
+    bool movementEnabled = true;
+
+
+	// Use this for initialization
+    void Start () {
+
+        anim = GetComponent<Animator> ();
+        playerRigidBody = GetComponent<Rigidbody2D> ();
+
+	}
+    
+    // Update is called once per frame
+    void Update () {
+        if (!movementEnabled) return;
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+	
+
+        playerMoving = false;
+
+        if (xInput > 0.5f || xInput < -0.5f) // Moving horizontally
+        {
+            playerRigidBody.velocity = new Vector2(xInput * moveSpeed, playerRigidBody.velocity.y);
+            anim.gameObject.transform.Translate(new Vector3(xInput * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(xInput, 0f);
+        }
+        else
+        {
+            playerRigidBody.velocity = new Vector2( 0f, playerRigidBody.velocity.y);
+        }
+
+        anim.SetFloat("MoveX", xInput);
+        anim.SetFloat("MoveY", yInput);
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+		int directionFacing;
+
+	}
+    
+   public void OnSpawnPlayer(Vector2 directionToFace)
+    {
+        lastMove = directionToFace;
+    }
+
+    public void EnableMovement(bool enable)
+    {
+        movementEnabled = enable;
+        if(!enable)
+        {
+            playerMoving = false;
+            playerRigidBody.velocity = new Vector2(0f, 0f);
+
+            anim.SetFloat("MoveX", 0f);
+            anim.SetFloat("MoveY", 0f);
+            anim.SetBool("PlayerMoving", playerMoving);
+        }
+    }
+
+}
