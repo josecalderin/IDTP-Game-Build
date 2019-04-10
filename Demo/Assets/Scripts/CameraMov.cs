d8b1d4da0d0922138c142474c146f9a543c484e8
Jose Calderin <ryancalderin97@gmail.com>
2019-04-10 14:42:45 -0400

    new version

---
+++ Demo/Assets/Scripts/CameraMov.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMov : MonoBehaviour
+
+{
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+    private int movingSpeed = (int)4f;
+
+        
+    void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime * movingSpeed;
+        {
+
+        }
+    }
+}
---
+++ Demo/Assets/Scripts/coinpickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinpickup : MonoBehaviour
+{
+
+    //public GameObject explosion;
+
+
+
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // If a missile hits this object
+        if (coll.gameObject.tag == "Player")
+        {
+            HudText.scoreValue += 1;
+            Debug.Log("HIT!");
+            Destroy(gameObject);
+
+        }
+
+    }
+}
+
